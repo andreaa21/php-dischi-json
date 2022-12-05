14f6a8f5a0c0bb667a9cc61a559d93f216ab3095
andreaa21 <andrea.zollini@proton.me>
2022-12-05 16:04:26 +0100

    axios api call

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ createApp({
 
     data(){
         return{
-
+            apiUrl:'server.php'
         }
+    },
+    methods:{
+        getRecord(){
+            axios.get(this.apiUrl)
+            .then(result => {
+                console.log(result.data);
+            })
+        }
+    },
+    mounted(){
+        console.log('----------');
     }
 
 }).mount('#app');
